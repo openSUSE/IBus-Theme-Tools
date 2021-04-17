4836fe623bcfa45a6b9f74af7c09e2c7caf87294
Hollow Man <hollowman@hollowman.ml>
2021-04-18 00:29:10 +0800

    Bugs fix; Output style change
    Signed-off-by: Hollow Man <hollowman@hollowman.ml>

--- IBus-Theme.py
+++ IBus-Theme.py
@@ -26,6 +26,13 @@ _ = gettext.gettext
 EXTENSION_URL = "https://extensions.gnome.org/extension/4112/customize-ibus/"
 SOURCE_CODE_URL = "https://github.com/HollowMan6/IBus-Theme"
 
+# Output Style
+BLACK_CYAN = "\033[1;30;46m"
+READ_YELLOW = "\033[1;31;43m"
+YELLOW_BLUE = "\033[1;33;44m"
+UNDER_LINE = "\033[4m"
+OUTPUT_END = "\033[0m"
+
 
 def getThemePathList():
     pathList = []
@@ -80,26 +87,32 @@ def changeGTKTheme():
     themeNameList.sort()
     count = 1
     while True:
-        print("\033[1;36;45m" +
-              _("Please select a GTK theme to apply for IBus:") + "\033[0m")
+        print(BLACK_CYAN +
+              _("Please select a GTK theme to apply for IBus:") + OUTPUT_END)
         for themeName in themeNameList:
-            print("[\033[1;36;45m"+str(count)+"\033[0m]\t"+themeName)
+            print("[" + BLACK_CYAN+str(count)+OUTPUT_END + "]\t" +
+                  UNDER_LINE + themeName + OUTPUT_END)
             count += 1
-        print("[\033[1;36;45mq\033[0m]\t\033[1;33;41m" + _("Exit") + "\033[0m")
-        selection = input("\033[1;33;44m" + _("(Empty to exit): ") + "\033[0m")
+        print("[" + BLACK_CYAN + "q" + OUTPUT_END + "]\t" +
+              READ_YELLOW + _("Exit") + OUTPUT_END)
+        selection = input(YELLOW_BLUE + _("(Empty to exit): ") + OUTPUT_END)
         if selection == "q" or not selection:
             print(_("Goodbye!"))
             exit(0)
         elif selection.isdigit() and int(selection) < count and int(selection) > 0:
             while True:
                 print(
-                    "\033[1;36;45m" + _("Please select a theme mode (Not guaranteed to work):") + "\033[0m")
-                print("[\033[1;36;45m0\033[0m]\t"+_("Default"))
-                print("[\033[1;36;45m1\033[0m]\t"+_("Light"))
-                print("[\033[1;36;45m2\033[0m]\t"+_("Dark"))
-                print("[\033[1;36;45mq\033[0m]\t\033[1;33;41m" + _("Exit") + "\033[0m")
+                    BLACK_CYAN + _("Please select a theme mode (Not guaranteed to work):") + OUTPUT_END)
+                print("[" + BLACK_CYAN + "0" + OUTPUT_END + "]\t" +
+                      UNDER_LINE + _("Default") + OUTPUT_END)
+                print("[" + BLACK_CYAN + "1" + OUTPUT_END + "]\t" +
+                      UNDER_LINE + _("Light") + OUTPUT_END)
+                print("[" + BLACK_CYAN + "2" + OUTPUT_END + "]\t" +
+                      UNDER_LINE + _("Dark") + OUTPUT_END)
+                print("[" + BLACK_CYAN + "q" + OUTPUT_END + "]\t" +
+                      READ_YELLOW + _("Exit") + OUTPUT_END)
                 modeSelection = input(
-                    "\033[1;33;44m" + _("(Empty to be default): ") + "\033[0m")
+                    YELLOW_BLUE + _("(Empty to be default): ") + OUTPUT_END)
                 if modeSelection == "q":
                     print(_("Goodbye!"))
                     exit(0)
@@ -118,10 +131,10 @@ def changeGTKTheme():
                     print(_("Done! Goodbye!"))
                     exit(0)
                 else:
-                    print("\033[1;33;41m" +
-                          _("Error: Wrong selection!") + "\033[0m\n")
+                    print(READ_YELLOW +
+                          _("Error: Wrong selection!") + OUTPUT_END + "\n")
         else:
-            print("\033[1;33;41m" + _("Error: Wrong selection!") + "\033[0m\n")
+            print(READ_YELLOW + _("Error: Wrong selection!") + OUTPUT_END + "\n")
             count = 1
 
 # For GNOME Desktop
@@ -197,8 +210,9 @@ def exportIBusGNOMEThemeCSS(styleSheet, recursive=False):
                     newCleanClassList = []
                     for cleanClass in cleanClassList:
                         if cleanClass == ".button":
-                            pageButtonContent += tinycss2.serialize(
-                                token.content)
+                            # For IBus button radius fix
+                            pageButtonContent += re.sub(r"\n(.+?)border-radius:(.+?);", "", tinycss2.serialize(
+                                token.content))
                         elif cleanClass.startswith(".button"):
                             newCleanClassList.append(cleanClass.replace(
                                 ".button", ".candidate-page-button"))
@@ -297,36 +311,39 @@ def exportIBusTheme():
     themeList.sort()
     count = 1
     while True:
-        print("\033[1;36;45m" +
-              _("Please select a GNOME theme to extract style sheet for IBus:") + "\033[0m")
+        print(BLACK_CYAN +
+              _("Please select a GNOME theme to extract style sheet for IBus:") + OUTPUT_END)
         for theme in themeList:
-            print("[\033[1;36;45m"+str(count)+"\033[0m]\t"+theme)
+            print("[" + BLACK_CYAN+str(count)+OUTPUT_END + "]\t" +
+                  UNDER_LINE + theme.replace("/gnome-shell/gnome-shell.css", "") + OUTPUT_END)
             count += 1
-        print("[\033[1;36;45mq\033[0m]\t\033[1;33;41m" + _("Exit") + "\033[0m")
-        selection = input("\033[1;33;44m" + _("(Empty to exit): ") + "\033[0m")
+        print("[" + BLACK_CYAN + "q" + OUTPUT_END + "]\t" +
+              READ_YELLOW + _("Exit") + OUTPUT_END)
+        selection = input(YELLOW_BLUE + _("(Empty to exit): ") + OUTPUT_END)
         if selection == "q" or not selection:
             print(_("Goodbye!"))
             exit(0)
         elif selection.isdigit() and int(selection) < count and int(selection) > 0:
-            print(
-                "\033[1;36;45m" + _("Please enter the path to store generated stylesheet:") + "\033[0m")
+            print("\n" +
+                  BLACK_CYAN + _("Please enter the path to store generated stylesheet:") + OUTPUT_END)
             path = input(
-                "\033[1;33;44m" + _("(Empty to be 'exportedIBusTheme.css' in current directory): ") + "\033[0m")
+                YELLOW_BLUE + _("(Empty to be 'exportedIBusTheme.css' in current directory): ") + OUTPUT_END)
             if not path:
                 path = "exportedIBusTheme.css"
             newCSS = exportIBusGNOMEThemeCSS(themeList[int(selection)-1])
             with open(path, "w") as f:
                 f.write(newCSS)
-            print("\n\033[1;33;44m" +
+            print(YELLOW_BLUE +
                   _("\nNow you can use Customize IBus GNOME Shell Extension:\n") + EXTENSION_URL)
-            print(_("to change IBus theme by selecting the extracted stylesheet."))
-            print(_("\nIf you make any changes to the extracted stylesheet after applying the file in above extension,"))
             print(
-                _("please press 'Alt + F2' and then 'r' or re-login to reload the GNOME-Shell to make the changes take effect.") + "\033[0m\n")
+                _("to change IBus theme by selecting the extracted stylesheet.") + OUTPUT_END)
+            print(YELLOW_BLUE + _("\nIf you make any changes to the extracted stylesheet after applying the file in above extension,"))
+            print(
+                _("please press 'Alt + F2' and then 'r' or re-login to reload the GNOME-Shell to make the changes take effect.") + OUTPUT_END)
             print(_("\nDone! Goodbye!"))
             exit(0)
         else:
-            print("\033[1;33;41m" + _("Error: Wrong selection!") + "\033[0m\n")
+            print(READ_YELLOW + _("Error: Wrong selection!") + OUTPUT_END + "\n")
             count = 1
 
 
@@ -334,29 +351,32 @@ if __name__ == "__main__":
     try:
         desktopEnv = os.environ["XDG_CURRENT_DESKTOP"].split(":")
     except Exception:
-        print("\033[1;33;41m" + _("Error: Not in Linux!") + "\033[0m")
+        print(READ_YELLOW + _("Error: Not in Linux!") + OUTPUT_END)
         exit(1)
     if "GNOME" in desktopEnv:
         exportIBusTheme()
     else:
         while True:
-            print("\033[1;36;45m" + _("Please select a mode:") + "\033[0m")
-            print("[\033[1;36;45m1\033[0m]\t"+_("Change GTK theme for IBus"))
-            print("[\033[1;36;45m2\033[0m]\t" +
-                  _("Extract an IBus-related GNOME theme stylesheet"))
-            print("[\033[1;36;45mq\033[0m]\t\033[1;33;41m" + _("Exit") + "\033[0m")
+            print(BLACK_CYAN + _("Please select a mode:") + OUTPUT_END)
+            print("[" + BLACK_CYAN + "1" + OUTPUT_END + "]\t" + UNDER_LINE +
+                  _("Change GTK theme for IBus") + OUTPUT_END)
+            print("[" + BLACK_CYAN + "2" + OUTPUT_END + "]\t" + UNDER_LINE +
+                  _("Extract an IBus-related GNOME theme stylesheet") + OUTPUT_END)
+            print("[" + BLACK_CYAN + "q" + OUTPUT_END + "]\t" +
+                  READ_YELLOW + _("Exit") + OUTPUT_END)
             modeSelection = input(
-                "\033[1;33;44m" + _("(Empty to be 1): ") + "\033[0m")
+                YELLOW_BLUE + _("(Empty to be 1): ") + OUTPUT_END)
             if modeSelection == "q":
                 print(_("Goodbye!"))
                 exit(0)
             elif modeSelection.isdigit() and int(modeSelection) >= 1 and int(modeSelection) <= 2 or not modeSelection:
                 if not modeSelection or modeSelection == "1":
+                    print("")
                     changeGTKTheme()
                 elif modeSelection == "2":
                     print(
-                        "\n\033[1;33;44m" + _("Note: You can only apply the stylesheet under GNOME.") + "\033[0m\n")
+                        "\n" + YELLOW_BLUE + _("Note: You can only apply the stylesheet under GNOME.") + OUTPUT_END + "\n")
                     exportIBusTheme()
             else:
-                print("\033[1;33;41m" +
-                      _("Error: Wrong selection!") + "\033[0m\n")
+                print(READ_YELLOW +
+                      _("Error: Wrong selection!") + OUTPUT_END + "\n")
